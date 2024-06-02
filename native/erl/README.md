c7285b1e8cbb8d438c2649a93170333831580ee0
voidfill <71205200+voidfill@users.noreply.github.com>
2024-06-02 18:57:44 +0200

    fix licensing for erlpack sysdep.h

--- native/erl/README.md
+++ native/erl/README.md
@@ -1,3 +1,1 @@
-TODO: Look at licensing for sysdep.h again.
-
 Native Node module for packing/unpacking etf encoded messages sent from/to discord gateway.
--- native/erl/sysdep.h
+++ native/erl/sysdep.h
@@ -1,5 +1,21 @@
-// taken from https://github.com/yukikaze-bot/erlpack/blob/main/cpp/sysdep.h
-// licensed under [Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0)
+
+/*!
+ * https://github.com/yukikaze-bot/erlpack/blob/main/cpp/sysdep.h
+ * Copyright 2021, 1chiSensei
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 
 #ifndef ERLPACK_SYSDEP_H__
 #define ERLPACK_SYSDEP_H__
